8e6999d84a81369223908ab67143356725e62a46
fabio <fabio.info@gmail.com>
2014-02-23 17:21:10 -0300

    a little refactoring

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,15 @@
 class ArticlesController < ApplicationController
 
   def index
-    @articles = 10.times.map do |index|
-      OpenStruct.new(title: "Article #{index.next}")
-    end
+    @articles = ["Article 1", "Article 2", "Article 3", "Article 4"]
 
     respond_to do |format|
       format.html
-      format.pdf { render :pdf => "articles-list-report", header: {center: "COMPANY NAME"}, footer: { center: "[page] of [topage]" } }
+
+      format.pdf { render pdf: "articles-list-report",
+        header: {center: "COMPANY NAME"},
+        footer: { center: "[page] of [topage]" }
+      }
     end
   end
 
